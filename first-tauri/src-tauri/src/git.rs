9d9ef9938ae62a0f143451769c57412e2ba1d62f
shlifedev <shlifedev@gmail.com>
2026-01-10 22:21:38 +0900

    chore : tokio 추가 및 String 인자 참조전달로 변경(ownership fix)

--- first-tauri/src-tauri/src/git.rs
+++ first-tauri/src-tauri/src/git.rs
@@ -1,37 +1,46 @@
 use std::process::Output;
 
-struct Git{
+struct Git;
 
-}
-
-impl Git{
-    async fn run_command(work_dir: String, command: String) -> (bool, Output) {
+impl Git {
+    async fn run_command(work_dir: &str, command: &str) -> (bool, Output) {
         println!("{work_dir}에서 {command} 명령어를 실행합니다.");
+
         let program = "git";
         let args = command.split_whitespace().collect::<Vec<&str>>();
-        let output = std::process::Command::new(program).args(&args).output().unwrap();
-        let mut has_error = false;
-        if(output.status.success()){
+
+        let output = tokio::process::Command::new(program)
+            .args(&args)
+            .current_dir(work_dir)
+            .output()
+            .await
+            .unwrap();
+
+        let has_error = !output.status.success();
+
+        if output.status.success() {
             let result = String::from_utf8_lossy(&output.stdout);
             println!("명령어 실행 결과: {}", result);
-            has_error = true;
         } else {
             let error = String::from_utf8_lossy(&output.stderr);
             println!("명령어 실행 오류: {}", error);
         }
-        return (has_error, output);
+
+        (has_error, output)
     }
 
-    fn resetHard(workDir: String){
-        println!("{workDir}의 변경사항을 모두 버립니다.");
-        Self::run_command(workDir, format!("reset --hard -f"));
+    async fn reset_hard(work_dir: &str) {
+        println!("{work_dir}의 변경사항을 모두 버립니다.");
+        Self::run_command(work_dir, "reset --hard").await;
     }
-    fn checkOut(workDir: String, branch: String, discardAll: bool){
-        println!("{workDir}의 {branch} 를 체크아웃합니다.");
 
-        // 리셋부터 실행하는게 나을듯
-        if(discardAll){
-           Self::resetHard(workDir);
+    async fn check_out(work_dir: &str, branch: &str, discard_all: bool) {
+        println!("{work_dir}의 {branch}를 체크아웃합니다.");
+
+        if discard_all {
+            Self::reset_hard(work_dir).await;
         }
+
+        Self::run_command(work_dir, &format!("checkout {}", branch)).await;
     }
 }
